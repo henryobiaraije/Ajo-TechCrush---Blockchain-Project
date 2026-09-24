a56823bcd19ace8eab6dc1ac8d1d6bff2f8d2f28
henryobiaraije <mpereere@gmail.com>
2026-09-24 23:02:30 +0100

    security: To ensure no one will ever call AjoRegistry directly, I've marked it as abstract. It can still be inherited by Ajo but can't be mistakenly deployed. I hope this will be enough for foundry to trigger an error if we mistakenly try to deploy it directly. Only Ajo should ever be deployed

--- src/registry/AjoRegistry.sol
+++ src/registry/AjoRegistry.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {AjoParticipant} from "../types/common.sol";
 
-contract AjoRegistry {
+abstract contract AjoRegistry {
 
     // =====================================================
     //                       ERRORS
