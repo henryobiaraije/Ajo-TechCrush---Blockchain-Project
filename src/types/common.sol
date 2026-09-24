d71aa20b711257d04740b9547f6944b87d2d938a
henryobiaraije <mpereere@gmail.com>
2026-09-24 21:34:57 +0100

    feat: Added the Ajo participant struct

---
+++ src/types/common.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache
+
+/// @notice Represents a single ajo participant
+    struct AjoParticipant {
+
+        /// @notice The address of this participant
+        address participantAddress;
+
+        /// @notice The serial number of this participant. Will be used to determine who go carry the pot and when serially.
+        uint256 serialNumber;
+
+        /// @notice The full name of this participant.
+        bytes32 participantFullName;
+    }
