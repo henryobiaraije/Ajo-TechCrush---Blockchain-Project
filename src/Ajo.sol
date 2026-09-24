f2acec36cf5084975512f7551e65e5c268972755
henryobiaraije <mpereere@gmail.com>
2026-09-24 20:49:53 +0100

    feat: Outlined the steps to take in join function and defined needed states

--- src/Ajo.sol
+++ src/Ajo.sol
@@ -1,6 +1,37 @@
 // SPDX-License-Identifier: Apache
 pragma solidity ^0.8.30;
 
-contract Ajo{
+contract Ajo {
+    /**
+     * @notice The maximum number of participants to participate in an ajo contribution at a time.
+     */
+    uint256 public constant MAXIMUM_AJO_PARTICIPANTS = 20;
 
+    /**
+     * @notice The fixed fee participants would need to pay to join.
+     */
+    uint256 public constant JOIN_FEE = 1 gwei;
+
+    /**
+     * @notice Tracks the total participants that has joined
+     */
+    uint8 public totalParticipants;
+
+    /**
+     * @notice Enables users to join this ajo contribution
+     * @return success True if the participant joined successfully.
+     */
+    function join() public payable returns (bool) {
+        // Ensure we have not exceeded the total number of participants.
+
+        // Ensure user doesn't exist yet (by account address).
+
+        // Ensure the user sent the join fee.
+
+        // Add the user as a participant.
+
+        // Track number of users who are joining.
+
+        // Track total number of participants that has joined.
+    }
 }
